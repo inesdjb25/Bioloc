a7e265b133e9d8694e071e57ea4d579e09ef4ba8
inesdjb25 <djebra.ines@gmail.com>
2021-03-30 15:22:57 +0200

    Lien entre page connexion et incription

--- src/components/Commercant/Commercant.js
+++ src/components/Commercant/Commercant.js
@@ -3,57 +3,41 @@ import "./Commercant.css";
 import { Link } from "react-router-dom";
 
 
-class Commercant extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {email: ''};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('Le nom a été soumis : ' + this.state.email);
-        event.preventDefault();
-      }
+class Commercant extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
 
-    render() {
-        console.log(this.state.email);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        return (
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-            <div className="card mb-5">
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
-                <div className="card-body">
-
-                <form onSubmit={this.handleSubmit}>
-                    <center>
-                     <p>Adresse mail </p>   
-                    <input type="text" className="form-control" id="input1" name="firstname" value={this.state.email} onChange={this.handleChange} placeholder="Adresse mail" />
-                    <p>Mot de passe </p> 
-                    <input type="text" className="form-control" id="input1" name="lastname" onChange={this.handleChange} placeholder="Mot de passe" />
-                   
-                    <button type="submit" className="btn btn-success bouton2">Se connecter</button>
-                    </center>
-
-                </form>
-                    
-                    
-                 </div>
-
-
-            
-            </div>
-            
-
-        );
-
-
-    }
+  render() {
+    return (
+      <div>
+       
+          <form onSubmit={this.handleSubmit}>
+          
+          
+        
+            <div className="form-group text-center">
+            <input type="text" className="form-control form-control-sm" name="mail" placeholder="Votre adresse email" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} value={this.state.value} onChange={this.handleChange}/>
+           <input type="password" className="form-control form-control-sm" name="motdepasse" placeholder="Mot de passe" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} />
+            <button className="btn btn-primary text-right" type="submit">Se connecter </button></div>
+          </form>
+          <p><a href="/Inscription">Pas encore de compte ? Venez vous inscrire</a> </p>
+        
+      </div>
+    );
+  }
 }
-
 export default Commercant;
--- src/components/Inscription/Inscription.js
+++ src/components/Inscription/Inscription.js
@@ -4,6 +4,22 @@ import { Link } from "react-router-dom";
 
 
 class Inscription extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({ value: event.target.value });
+      }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+      }
     render() {
 
         return (
@@ -14,6 +30,24 @@ class Inscription extends Component {
                     <p className="lead">
                        Incrivez-vous et découvrez les avantages que vous gagnerez
                     </p>
+                    <form onSubmit={this.handleSubmit}>
+          <input type="text" className="form-control form-control-sm" name="prénom" placeholder="Prénom"  autoComplete="on" required minLength={2} value={this.state.value} onChange={this.handleChange} />
+          <input type="text" className="form-control form-control-sm" name="nom" placeholder="Nom" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} />
+          <input type="text" className="form-control form-control-sm" name="mail" placeholder="Votre adresse email" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} />
+          <input type="password" className="form-control form-control-sm" name="motdepasse" placeholder="Mot de passe" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} />
+          <input type="text" className="form-control form-control-sm" name="prénom" placeholder="Prénom" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} />
+            <h6 className="text-center" style={{ width: '399px', height: '28.2px', marginTop: '51px' }}>Votre commerce</h6>
+            <input type="text" className="form-control form-control-sm" name="nomcommerce" placeholder="Nom de votre commerce" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} />
+            <div className="form-group"><textarea className="form-control form-control-sm" name="Descripition de votre commerce" placeholder="Descripition de votre commerce" rows={14} defaultValue={""} /></div>
+            <small>Ajouter l'icône de votre commerce</small><input type="file" className="form-control-file" name="logo" placeholder="Prénom" style={{ padding: '3px 8px 4px -6px', borderWidth: '2px', margin: '0px', marginTop: '10px', marginBottom: '9px', borderRadius: '7px', boxShadow: '0px 0px var(--indigo)' }} autoComplete="on" required minLength={2} /><em>Italic</em><strong>Bold</strong>
+            <blockquote className="blockquote">
+              <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+              <footer className="blockquote-footer">Someone famous</footer>
+            </blockquote>
+            <p>Paragraph</p>
+            <div className="form-group text-center">
+            <button className="btn btn-primary text-right" type="submit">Envoyer </button></div>
+          </form>
                 
             </div>
 
